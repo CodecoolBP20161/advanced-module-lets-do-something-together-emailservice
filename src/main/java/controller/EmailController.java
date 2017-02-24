89208e653be24121d9cd85de1d53fc7b3ff5c4c8
cickib <bellakocsis06@gmail.com>
2017-02-24 10:48:08 +0100

    LSD-47 template and subject sent in request

--- src/main/java/controller/EmailController.java
+++ src/main/java/controller/EmailController.java
@@ -8,12 +8,7 @@ import model.Sender;
 import service.EmailService;
 import spark.Request;
 import spark.Response;
-import spark.utils.IOUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +17,6 @@ import java.util.List;
 public class EmailController {
     private static EmailAddressDao emailAddressDao = EmailAddressDaoImpl.getInstance();
     private final EmailService service;
-    private final String emailSubject = "Welcome to ActiMate";
 
     public EmailController(EmailService service) {
         this.service = service;
@@ -40,7 +34,7 @@ public class EmailController {
         if (addresses != null) {
             addresses.stream()
                     .filter(address ->
-                            service.send(createEmail(address)))
+                            service.send(createEmail(address, req)))
                     .forEach(address ->
                             emailAddressDao.switchSentStatus(address));
         }
@@ -56,13 +50,7 @@ public class EmailController {
         return String.join(",", addresses);
     }
 
-    private Email createEmail(String recipient) {
-        StringWriter writer = new StringWriter();
-        try {
-            IOUtils.copy(new FileInputStream(new File("./src/main/resources/templates/email.html")), writer);
-        } catch (IOException e) {
-            e.getMessage();
-        }
-        return new Email(new Sender(), recipient, emailSubject, writer.toString());
+    private Email createEmail(String recipient, Request req) {
+        return new Email(new Sender(), recipient, req.queryParams("subject"), req.queryParams("template"));
     }
 }
