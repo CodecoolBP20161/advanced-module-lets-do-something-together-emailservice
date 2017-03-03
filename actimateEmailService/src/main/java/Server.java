527c3d40261021d4452341efa88fda3cba4a7bca
cickib <bellakocsis06@gmail.com>
2017-03-03 18:44:30 +0100

    emails sent every half minutes

--- actimateEmailService/src/main/java/Server.java
+++ actimateEmailService/src/main/java/Server.java
@@ -2,10 +2,14 @@ import controller.EmailController;
 import dao.DbHandler;
 import service.EmailService;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static spark.Spark.*;
 
 
 public class Server {
+    private static final Timer timer = new Timer();
     private static EmailController emailController;
 
     public static void main(String[] args) {
@@ -17,5 +21,17 @@ public class Server {
 
         get("/", emailController::saveAdresses);
         get("/sent", emailController::sentAddresses);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                try{
+                    emailController.sendToAddresses();
+                } catch (NullPointerException e){
+                    e.getMessage();
+                }
+            }
+        };
+        timer.schedule(timerTask, 1000, 30000);
     }
 }
